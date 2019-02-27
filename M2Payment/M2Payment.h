e6f321b336589a4aa83b1431b939d2c056b94c81
Robby Michels <Robby.Michels@hva.nl>
2019-02-27 09:06:09 +0100

    Initial Commit

---
+++ M2Payment/M2Payment.h
@@ -0,0 +1,19 @@
+//
+//  M2Payment.h
+//  M2Payment
+//
+//  Created by Robby Michels on 27/02/2019.
+//  Copyright © 2019 Robby Michels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M2Payment.
+FOUNDATION_EXPORT double M2PaymentVersionNumber;
+
+//! Project version string for M2Payment.
+FOUNDATION_EXPORT const unsigned char M2PaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M2Payment/PublicHeader.h>
+
+
---
+++ M2PaymentTests/M2PaymentTests.swift
@@ -0,0 +1,34 @@
+//
+//  M2PaymentTests.swift
+//  M2PaymentTests
+//
+//  Created by Robby Michels on 27/02/2019.
+//  Copyright © 2019 Robby Michels. All rights reserved.
+//
+
+import XCTest
+@testable import M2Payment
+
+class M2PaymentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
